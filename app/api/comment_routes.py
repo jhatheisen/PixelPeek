e88f7b78ed25501d59827ed21d7460ed185bb09a
Shana Edouard <shanaedouard06@gmail.com>
2023-02-12 02:08:26 -0800

    put route added, photos/photo.id/comments routes removed

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import Comment, Photo, User
+from app.models import Comment, Photo, User, db
+from app.forms import CommentForm
 
 comment_routes = Blueprint('comments', __name__)
 
-@comment_routes.route('current')
+@comment_routes.route('/current')
 @login_required
 def user_comments(user_id=2):
     comments = Comment.query.all()
@@ -12,22 +13,27 @@ def user_comments(user_id=2):
         comments = Comment.query.filter_by(user_id=user_id).all()
     return {'Comments': [comment.to_dict() for comment in comments]}
 
-# @comment_routes.route('/')
-@comment_routes.route('api/photos/<int:photo_id>/comments')
-def all_comments(photo_id):
-    comments = Comment.query.filter_by(photo_id=photo_id).all()
-    return {'Comments': [comment.to_dict() for comment in comments]}
-
-
-@comment_routes.route('/photos/<int:photo_id>/comments', methods=['POST'])
-@login_required
-def create_comments(photo_id):
-    pass
-
 @comment_routes.route('<int:comment_id>', methods=['PUT'])
 @login_required
 def edit_comments(comment_id):
-    pass
+    editComments = db.session.query(Comment).get(int(comment_id))
+    data = request.get_json()
+    form = CommentForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit:
+        editComments.comment = form.data["comment"]
+
+        db.session.commit()
+
+    return {
+        "id": comment_id,
+        "comment": form.data["comment"],
+        "user_id": editComments.user_id,
+        "photo_id": editComments.photo_id,
+        "createdAt": editComments.createdAt,
+    }
+
 
 @comment_routes.route('<int:comment_id>', methods=['DELETE'])
 @login_required
--- app/models/comments.py
+++ app/models/comments.py
@@ -10,7 +10,7 @@ class Comment(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    comment = db.Column(db.String(250), nullable=False)
+    comment = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
     photo_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("photos.id")), nullable=False)
     createdAt = db.Column(db.DateTime, nullable=False, default=today)
