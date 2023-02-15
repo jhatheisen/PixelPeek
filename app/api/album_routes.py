926d4efd146a3cb0359d8b298354638ba82da8ce
jlin231 <jonlin981@gmail.com>
2023-02-15 13:36:51 -0800

    finished crud for albums

--- app/api/album_routes.py
+++ app/api/album_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import db, Album, album_photos
+from app.models import db, Album, album_photos, Photo
 from .auth_routes import validation_errors_to_error_messages
 from ..forms.album_form import CreateAlbumForm
 
@@ -54,6 +54,7 @@ def create_album():
   form = CreateAlbumForm()
   form['csrf_token'].data = request.cookies['csrf_token']
 
+
   if form.validate_on_submit():
     data = form.data
 
@@ -61,47 +62,72 @@ def create_album():
         album_name= data["album_name"],
         user_id = current_user.id
     )
+    
 
     db.session.add(newAlbum)
     db.session.commit()
     print("-------------------<SUCCESS")
 
-    allPhotos = Photo.query.all()
-
-
     return {
-        "id": allPhotos[len(allPhotos)-1].id,
-        "user_id": user_id,
-        "title": data["title"],
-        "description": data["description"],
-        "city": data["city"],
-        "state": data["state"],
-        "country": data["country"],
-        "img_url": data["img_url"],
-        "createdAt": allPhotos[len(allPhotos)-1].createdAt
+        "album_name": newAlbum.album_name
     }
   return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
 
-# @comment_routes.route('/<int:comment_id>', methods=['DELETE'])
-# @login_required
-# def delete_comments(comment_id):
-#     comment = Comment.query.get(comment_id)
+@album_routes.route('/<int:albumId>', methods=['PUT'])
+@login_required
+def add_photo_to_album(albumId):
+    #Check if current user is owner of album and owner of photoId
+    data = request.get_json()
+    photoId = data["photoId"]
+    
+    print(photoId)
 
-#     if comment is None:
-#         return {
-#             "message": "Comment couldn't be found",
-#             "statusCode": 404
-#         }, 404
+    singlePhoto = db.session.query(Photo).get(photoId)
+    singleAlbum = db.session.query(Album).get(albumId)
 
-#     if comment.user_id != current_user.id:
-#         return {'errors': ['Unauthorized']}, 401
+    if singlePhoto is None:
+        return {
+            "message": "Photo couldn't be found",
+            "statusCode": 404
+        }, 404
 
-#     if comment is not None:
-#         db.session.delete(comment)
-#         db.session.commit()
-#         return {
-#             "message": "Successfully deleted",
-#             "statusCode": 200
-#         }, 200
+    if singleAlbum.user_id != current_user.id:
+        return {'errors': ['Unauthorized']}, 401
+
+    if current_user.id != singlePhoto.user_id:
+        return {'errors': ['Unauthorized']}, 401
+
+    #execute update
+    print("photos before here=================>",singleAlbum.photos)
+    singleAlbum.photos.append(singlePhoto)
+    print("photos after here=================>",singleAlbum.photos)
+    db.session.commit()
+
+    return {
+        "message": "successfully added"
+    }
+
+
+@album_routes.route('/<int:albumId>', methods=["DELETE"])
+@login_required
+def delete_album(albumId):
+    album = db.session.query(Album).get(albumId)
+    
+    if album is None:
+        return {
+            "message": "Comment couldn't be found",
+            "statusCode": 404
+        }, 404
+
+    if album.user_id != current_user.id:
+        return {'errors': ['Unauthorized']}, 401
+    
+    if album is not None:
+        db.session.delete(album)
+        db.session.commit()
+        return {
+            "message": "Successfully deleted",
+            "statusCode": 200
+        }, 200
