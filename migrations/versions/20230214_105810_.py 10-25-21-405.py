64c435d735082ec531571338f5c22fea6739866e
jlin231 <jonlin981@gmail.com>
2023-03-13 16:48:41 -0700

    added photos

--- migrations/versions/20230214_105810_.py 10-25-21-405.py
+++ migrations/versions/20230214_105810_.py 10-25-21-405.py
@@ -83,6 +83,7 @@ def upgrade():
 
     if environment == "production":
         op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE photos SET SCHEMA {SCHEMA};")
         op.execute(f"ALTER TABLE albums SET SCHEMA {SCHEMA};")
         op.execute(f"ALTER TABLE comments SET SCHEMA {SCHEMA};")
         op.execute(f"ALTER TABLE tags SET SCHEMA {SCHEMA};")
